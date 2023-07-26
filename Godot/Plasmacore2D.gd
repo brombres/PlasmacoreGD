0de070242bf2ff6bfdf1292848b227802a7aaed0
Brom Bresenham <brom.bresenham@runegate.com>
2023-07-26 15:01:59 -0400

    [GDObject,Graphic] GDObject.load_scene(), Graphic improvements

--- Godot/Plasmacore2D.gd
+++ Godot/Plasmacore2D.gd
@@ -2,7 +2,9 @@ class_name Plasmacore2D
 extends Node2D
 
 func _ready():
-	var assets = $Assets
-	if assets: assets.visible = false
+	print("------------")
+	if is_visible:
+		var assets = get_node_or_null("Assets")
+		if assets: assets.visible = false
 
-	Plasmacore.on_new_scene( 2, self )
+		Plasmacore.on_new_scene( 2, self )
